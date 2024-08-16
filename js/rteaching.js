a1255989741a6a9624470757fe224e366f49cf68
tjaymz <tjaymz@gmail.com>
2024-08-16 13:59:01 -0400

    Changed the default size of the videos when displayed on the page

--- js/rteaching.js
+++ js/rteaching.js
@@ -43,8 +43,8 @@ function loadVideos() {
 
 function createYoutubeIframe(videoId) {
   const iframe = document.createElement('iframe');
-  iframe.width = "560";
-  iframe.height = "315";
+  iframe.width = "355";
+  iframe.height = "200";
   iframe.src = `https://www.youtube.com/embed/${videoId}`;
   iframe.title = "YouTube video player";
   iframe.frameBorder = "0";
